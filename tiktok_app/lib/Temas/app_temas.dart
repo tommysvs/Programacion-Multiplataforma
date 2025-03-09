2042eb006230ea76c33469ff943efbbad1437af8
Tommy Vega <tommysvs@hotmail.com>
2025-03-08 18:21:01 -0600

    se crean archivos y carpetas necesarias para la app

---
+++ tiktok_app/lib/Temas/app_temas.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppTemas {
+  ThemeData getTheme() =>
+      ThemeData(useMaterial3: true, brightness: Brightness.dark);
+}
---
+++ tiktok_app/lib/domain/entities/video_post.dart
@@ -0,0 +1,13 @@
+class VideoPost {
+  final String caption;
+  final String videoUrl;
+  final int likes;
+  final int views;
+
+  VideoPost({
+    required this.caption,
+    required this.videoUrl,
+    required this.likes,
+    required this.views,
+  });
+}
